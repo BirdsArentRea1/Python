824d2d5b6a87609cc7becbcd4fbba0d6abd92334
BirdsArentRea1 <146360250+BirdsArentRea1@users.noreply.github.com>
2024-02-09 11:32:52 -0700

    Create forloop1.py

---
+++ forloop1.py
@@ -0,0 +1,25 @@
+#0-12
+for i in range (0, 13):
+    print(i, end =" ")
+    
+print()
+    
+#8-17
+for i in range (8, 18):
+    print(i, end =" ")
+    
+print()
+    
+#20-50 (-5)
+for i in range (20, 51, 5):  
+    print(i, end =" ")
+    
+print()
+    
+#10--6 (-2)
+for i in range (10, -8, -2):
+    print(i, end =" ")
+    
+print()
+
+    
