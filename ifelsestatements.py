90350cdd300aa96acec3e05b3283bd920a905bbf
BirdsArentRea1 <146360250+BirdsArentRea1@users.noreply.github.com>
2024-02-09 11:30:58 -0700

    Create ifelsestatements.py

---
+++ ifelsestatements.py
@@ -0,0 +1,22 @@
+#pbj
+pbj = input("Do you like PB sandwiches (y/n)")
+if pbj == 'y':
+    print("ITS PEANUT BUTTER JELLY TIME!!!")
+
+#fahrenheit
+fahrenheit=int(input("How cold is it in fahrenheit?"))
+if fahrenheit < 60:
+    print("You should wear a jacket")
+else:
+    print("A T-shirt is fine")
+    
+#pets
+pets = input("Do you prefer Cats, Dogs or Fish?")
+if pets == "Cats":
+        print("meow")
+elif pets == "Dogs":
+        print("woof")
+elif pets == "Fish":
+        print("L")
+else:
+        print("Not an option.")
