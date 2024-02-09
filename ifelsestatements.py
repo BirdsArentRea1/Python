61e41cd1c57801e19154b2c7193c3912e88510ca
BirdsArentRea1 <146360250+BirdsArentRea1@users.noreply.github.com>
2024-02-09 11:32:02 -0700

    Update ifelsestatements.py

--- ifelsestatements.py
+++ ifelsestatements.py
@@ -20,3 +20,26 @@ elif pets == "Fish":
         print("L")
 else:
         print("Not an option.")
+
+#cats
+cats = input("Do you like cats/ (y/n)")
+if cats == 'y':  
+    print("meow")
+    
+#inches
+inches = int(input("How many inches tall are you?"))
+if inches < 48:
+        print("you are too short to ride the ferris wheel.")
+else:
+        print("you are tall enough to ride the ferris wheeel.")
+        
+#pokemon
+choice = input("Do you prefer (P)ikachu, (B)ulbasaur, or (C)harizard")
+if choice == 'P':
+    print("Pika Pika")
+elif choice == 'B':
+    print("...............")
+elif choice == 'C':
+    print("vjgdfjesoijf")
+else:
+    print("how did you possibly mess up answering the question there were three answers")
