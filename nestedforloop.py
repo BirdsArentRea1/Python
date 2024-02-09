b74f3fd62814ba6ea9ee2a3ffd3d81f104963c54
BirdsArentRea1 <146360250+BirdsArentRea1@users.noreply.github.com>
2024-02-09 08:33:37 -0700

    Create nestedforloop.py

---
+++ nestedforloop.py
@@ -0,0 +1,17 @@
+print("problem 1")
+for i in range(2): #rows
+    for j in range(3): #columns
+        print("*" ,end = " ") 
+    print()
+        
+print("problem 2")
+for i in range(2): #rows
+    for j in range(5): #columns
+        print(j + 1, end = " ") 
+    print()
+        
+print("problem 3")
+for i in range(5): #rows
+    for j in range(i): #columns
+        print("*", end = " ") 
+    print()
