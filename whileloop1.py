4b424b10dea45c4b725f3f94c503f3a6268de80a
BirdsArentRea1 <146360250+BirdsArentRea1@users.noreply.github.com>
2024-02-09 11:33:27 -0700

    Create whileloop1.py

---
+++ whileloop1.py
@@ -0,0 +1,23 @@
+#while loop untill bless you
+choice = "AH  CHOO!"
+while choice != "bless you":
+    print(choice)
+    choice = input("type bless you to quit")
+    
+print("goodbye.")
+
+print()
+
+#20-200 by 10
+num = 20
+while num <= 200:
+    print(num, end = " ")
+    num+=10
+    
+print()
+    
+#80->40 by -5
+num = 80
+while num <= 40:
+    print(num, end = " ")
+    num+=5
