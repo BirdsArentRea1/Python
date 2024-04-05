c0ff45fbd564cdbcccbc53d7b226a4bcefc3d32d
BirdsArentRea1 <146360250+BirdsArentRea1@users.noreply.github.com>
2024-04-05 09:09:57 -0600

    Update FishingSim.py

--- FishingSim.py
+++ FishingSim.py
@@ -1,22 +1,58 @@
-import random
+#include <iostream>
+#include <windows.h>  
+#include <chrono>
+#include <thread>
+#include <random>
+using namespace std;
+using namespace std::this_thread; // This line already includes the necessary functionality
+using namespace std::chrono_literals;
+using std::chrono::system_clock;
 
-def fishing(): #start of function definition
-    num = random.randrange(0, 100) #produces a number b/t 0-99
-    if num < 10:
-        print("you caught algae!")
-        return "algae"
-    if num < 20:
-        print("you caught a trash!")
-        return "trash"
-    if num < 33:
-        print("you caught a carp!")
-        return "carp"
-    if num < 66:
-        print("you caught a woodskip!")
-        return "woodskip"
-    if num < 99:
-        print("you caught a catfish!")
-        return "catfish"
-    
-#function call---------------------------------------------
-print(fishing(), "added to your inventory")
+string inventory[10];
+void Fishing();
+
+// Random number generator setup
+random_device rd;
+mt19937 gen(rd());
+uniform_int_distribution<> distr(1, 5); // Random between 1 and 5 seconds
+
+int main() {
+    char input = 'a';
+    int room = 1;
+
+    while (input != 'q') {
+        cout << "your inventory:" << endl;
+        for (int i = 0; i < 10; i++)
+            cout << inventory[i] << " ";
+        cout << endl;
+
+        switch (room) {
+        case 1:
+            cout << "Press F to fish" << endl;
+            cin >> input;
+            if (input == 'F' || input == 'f') {
+                cout << "fishing..." << endl;
+                int random_seconds = distr(gen); // Generate a random number for sleep duration
+                sleep_for(chrono::seconds(random_seconds)); // Use std::chrono::seconds
+                Fishing();
+            }
+            break;
+        }
+    }
+}
+
+void Fishing() {
+    int num = rand() % 100;
+    if (num < 33) {
+        cout << "You got a carp" << endl;
+        inventory[0] = "carp";
+    }
+    else if (num < 66) {
+        cout << "You got a Woodskip" << endl;
+        inventory[1] = "woodskip";
+    }
+    else if (num < 99) {
+        cout << "You got a Catfish" << endl;
+        inventory[2] = "catfish";
+    }
+}
